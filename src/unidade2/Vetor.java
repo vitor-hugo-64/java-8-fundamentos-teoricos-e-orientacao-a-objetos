226ddaeae4e386d91ee0852efcc2b5576ed7dec9
vitor-hugo-64 <vitorhugooliveira64@gmail.com>
2018-03-21 18:24:18 -0300

    Vetor

--- src/unidade2/Vetor.java
+++ src/unidade2/Vetor.java
@@ -1,9 +1,22 @@
 package unidade2;
 
+import java.util.Random;
+
 public class Vetor {
-	
+
 	public static void main(String[] args) {
 		
+		int[] vetor = new int[3];
+		
+		Random gerador = new Random();
+		
+		for(int i = 0; i <= vetor.length; i++) {
+			vetor[i] = gerador.nextInt();
+		}
+		
+		for(int value: vetor) {
+			System.out.println(value);
+		}
 		
 		
 	}
